e27724de301c19119b5913eda984e7b58456a71f
Rob Norris <rob_norris@mac.com>
2020-10-01 13:29:24 -0500

    rename

--- modules/core/src/main/scala/Config.scala
+++ modules/core/src/main/scala/Config.scala
@@ -9,7 +9,7 @@ import cats.data._
 import cats.implicits._
 // import cats.effect.Sync
 
-sealed abstract case class Env[A](run: (String => Option[String]) => (Trace, Option[A])) { outer =>
+sealed abstract case class Config[A](run: (String => Option[String]) => (Trace, Option[A])) { outer =>
 
   // def load[F[_]: Sync]: F[A] =
   //   Sync[F].delay(run((sys.env orElse sys.props).lift)).flatMap {
@@ -28,8 +28,8 @@ sealed abstract case class Env[A](run: (String => Option[String]) => (Trace, Opt
   //     ))
   //   }
 
-  def emap[B](f: A => Either[String, B]): Env[B] =
-    Env { env =>
+  def emap[B](f: A => Either[String, B]): Config[B] =
+    Config { env =>
       run(env) match {
         case (t, None)    => (t, None)
         case (t, Some(a)) =>
@@ -40,23 +40,23 @@ sealed abstract case class Env[A](run: (String => Option[String]) => (Trace, Opt
       }
     }
 
-  def option: Env[Option[A]] =
+  def option: Config[Option[A]] =
     optional
 
-  def optional: Env[Option[A]] =
-    Env { env =>
+  def optional: Config[Option[A]] =
+    Config { env =>
       val (t, op) = run(env)
       (Trace.Optional(t, op.isDefined), Some(op).filter(_ => t.errors.nonEmpty))
     }
 
-  def to[B](implicit ev: EMapper[A, B]): Env[B] =
+  def to[B](implicit ev: EMapper[A, B]): Config[B] =
     emap(ev.apply)
 
   // TODO: remove
-  def as[B](implicit ev: EMapper[A, B]): Env[B] =
+  def as[B](implicit ev: EMapper[A, B]): Config[B] =
     to[B]
 
-  def or(that: Env[A]): Env[A] =
+  def or(that: Config[A]): Config[A] =
     this <+> that
 
   // def default(value: A): Env[A] =
@@ -69,24 +69,24 @@ sealed abstract case class Env[A](run: (String => Option[String]) => (Trace, Opt
 
 }
 
-object Env {
+object Config {
 
   private[envy] def apply[A](run: (String => Option[String]) => (Trace, Option[A])) =
-    new Env[A](run) {}
+    new Config[A](run) {}
 
-  implicit val MonadErrorEnv: MonadError[Env, NonEmptyChain[String]] with SemigroupK[Env] =
-    new MonadError[Env, NonEmptyChain[String]] with SemigroupK[Env] {
+  implicit val MonadErrorEnv: MonadError[Config, NonEmptyChain[String]] with SemigroupK[Config] =
+    new MonadError[Config, NonEmptyChain[String]] with SemigroupK[Config] {
 
-      def pure[A](a: A): Env[A] =
-        Env(_ => (Trace.Pure, Some(a)))
+      def pure[A](a: A): Config[A] =
+        Config(_ => (Trace.Pure, Some(a)))
 
-      def raiseError[A](messages: NonEmptyChain[String]): Env[A] =
-        Env { _ =>
+      def raiseError[A](messages: NonEmptyChain[String]): Config[A] =
+        Config { _ =>
           (messages.map[Trace](Trace.Error(_)).reduceLeft(_ && _), None)
         }
 
-      def handleErrorWith[A](fa: Env[A])(f:  NonEmptyChain[String] => Env[A]): Env[A] =
-        Env { env =>
+      def handleErrorWith[A](fa: Config[A])(f:  NonEmptyChain[String] => Config[A]): Config[A] =
+        Config { env =>
           fa.run(env) match {
             case (t, Some(a)) => (t, Some(a))
             case (t, None) =>
@@ -97,14 +97,14 @@ object Env {
           }
         }
 
-      override def map[A, B](fa: Env[A])(f: A => B): Env[B] =
-        Env { env =>
+      override def map[A, B](fa: Config[A])(f: A => B): Config[B] =
+        Config { env =>
           val (t, a) = fa.run(env)
           (t, a.map(f))
         }
 
-      def flatMap[A, B](fa: Env[A])(f: A => Env[B]): Env[B] =
-        Env { env =>
+      def flatMap[A, B](fa: Config[A])(f: A => Config[B]): Config[B] =
+        Config { env =>
           fa.run(env) match {
             case (t, None)    => (t && Trace.Halt, None)
             case (t, Some(a)) =>
@@ -114,8 +114,8 @@ object Env {
           }
         }
 
-      def tailRecM[A, B](a: A)(f: A => Env[Either[A,B]]): Env[B] =
-        Env { env =>
+      def tailRecM[A, B](a: A)(f: A => Config[Either[A,B]]): Config[B] =
+        Config { env =>
 
           def go(t: Trace, op: Option[Either[A, B]]): (Trace, Option[B]) =
             op match {
@@ -131,8 +131,8 @@ object Env {
 
         }
 
-      def combineK[A](x: Env[A], y: Env[A]): Env[A] =
-        Env { env =>
+      def combineK[A](x: Config[A], y: Config[A]): Config[A] =
+        Config { env =>
           val (tx, ax) = x.run(env)
           val (ty, ay) = y.run(env)
           (tx || ty, ax <+> ay)
@@ -140,13 +140,13 @@ object Env {
 
     }
 
-  implicit val ParallelEnv: Parallel[Env] =
-    new Parallel[Env] {
+  implicit val ParallelEnv: Parallel[Config] =
+    new Parallel[Config] {
       type F[A] = Par[A]
-      val sequential  = new (Par ~> Env) { def apply[A](fa: Par[A]) = Env(fa.run) }
-      val parallel    = new (Env ~> Par) { def apply[A](fa: Env[A]) = Par(fa.run) }
+      val sequential  = new (Par ~> Config) { def apply[A](fa: Par[A]) = Config(fa.run) }
+      val parallel    = new (Config ~> Par) { def apply[A](fa: Config[A]) = Par(fa.run) }
       val applicative = Par.ApplicativeParEnv
-      val monad       = Env.MonadErrorEnv
+      val monad       = Config.MonadErrorEnv
    }
 
 
